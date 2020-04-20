ce02e9a8690e69000666ad2a46866da9e5712a7c
Radovan Janjic <radovan.janjic@trivago.com>
2020-04-20 08:33:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import Card from './Card';
+import CardModule from './CardModule/Card';
+import imgUrl from './chuck.png';
+
+const facts = [
+    "Chuck Norris doesn't bug hunt as that signifies a probability of failure, he goes bug killing.",
+    'Once a police officer caught Chuck Norris, the cop was lucky enough to escape with a warning.',
+    "Chuck Norris doesn't step on toes. Chuck Norris steps on necks.",
+    'Chuck Norris can divide by zero.',
+    "Chuck Norris doesn't need an account. He just logs in.",
+    'Chuck Norris won super bowls VII and VIII singlehandedly before unexpectedly retiring to pursue a career in ass-kicking.',
+    "With Chuck Norris P = NP. There's no nondeterminism with Chuck Norris decisions.",
+    'There are only two things that can cut diamonds: other diamonds, and Chuck Norris.',
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [facts, setFact] = useState([]);
+
+    // useEffect(() => {
+    //     const interval = setInterval(async () => {
+    //         const response = await fetch('https://api.icndb.com/jokes/random');
+    //         const json = await response.json();
+    //         setJokes((facts) => [json.value.joke, ...facts]);
+    //     }, 1000);
+    //     return () => clearInterval(interval);
+    // }, []);
+
+    return (
+        <div className="flex flex-col flex-grow-0 justify-center">
+            {facts.map((joke, key) => (
+                <CardModule
+                    headline={'Chuck Norris Fact'}
+                    image={imgUrl}
+                    text={joke}
+                    key={key}
+                />
+            ))}
+
+            {facts.map((joke, key) => (
+                <Card
+                    headline={'Chuck Norris Fact'}
+                    image={imgUrl}
+                    text={joke}
+                    key={key}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default ({ headline, text, image }) => (
+    <div className="w-full mx-auto mt-4 flex flex-grow-0 p-6 bg-white rounded-lg shadow-xl">
+        <div className="flex-shrink-0">
+            <img
+                src={image}
+                alt=""
+                className="App-logo h-12 w-12 rounded-full"
+            />
+        </div>
+        <div className="ml-6 pt-1">
+            <h4 className="text-xl text-gray-700 xxl:text-gray-600">
+                {headline}
+            </h4>
+            <p className="text-base text-gray-600">{text}</p>
+        </div>
+    </div>
+);
